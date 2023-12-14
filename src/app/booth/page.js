f58ff7895b2a29a1661e3dd16ee2f567575b0e14
KaymanLab <kayman.nar@gmail.com>
2023-12-14 15:44:10 -0500

    test

--- src/app/booth/page.js
+++ src/app/booth/page.js
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline';
 
 export default function Booth(){
 
-    return (
+return (
         <div>
             <h1>10x10</h1>
             <Spline scene="https://prod.spline.design/snxj9qZEo4Dq9B7T/scene.splinecode" />
