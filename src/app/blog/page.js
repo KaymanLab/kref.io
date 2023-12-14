283a0734e0176a00c6cd4d538374f81da6324f8f
KaymanLab <kayman.nar@gmail.com>
2023-12-13 20:57:13 -0500

    add card

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,10 +1,12 @@
 import getDomain from "@/app/lib/getDomain"
+import BlogCard from './card'
 
 async function getData(){
     // 1 endpoint - API
     const domain = getDomain()
     const endpoint = `${domain}/api/posts` // -> third party api request??
-    const res = await fetch(endpoint, {next: {revalidate: 10}} ) //HTTP GET Method
+    const res = await fetch(endpoint, {next: {cache: 'no-store'}} )
+    // const res = await fetch(endpoint, {next: {revalidate: 10}} ) //HTTP GET Method
 
     
     if (!res.ok) {
@@ -22,13 +24,12 @@ async function getData(){
 export default async function BlogPage(){
     const data = await getData()
     const items = data && data.items ? [...data.items] : []
-    console.log(items)
-    console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
+   
     return <main>
         <h1>Hello World</h1>
         <p>Posts: </p>
         {items && items.map((item, idx)=>{
-            return <li key={`post-${idx}`}>{item.title}</li>
+            return <BlogCard title={item.title} key={`post-${idx}`} />
         })}
     </main>
 
